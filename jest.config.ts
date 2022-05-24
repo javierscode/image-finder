548f9eea0c8c245499fa39eacca483f7549db2e9
Javier Linares <javihp79@gmail.com>
2022-05-24 15:44:04 +0200

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.(css)$': 'identity-obj-proxy',
+  },
+}
